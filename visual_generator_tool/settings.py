bf4fe651fca07296c16727b037b4b746ac18478a
MarketingBot <golakiyavraj2204@gmail.com>
2025-09-12 16:34:07 +0530

    DB_HOST

--- visual_generator_tool/settings.py
+++ visual_generator_tool/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'HOST': os.getenv('DB_HOST', '69.62.78.167'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
