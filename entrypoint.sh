c82c1b82a82826330d12466314413873161e6443
MarketingBot <golakiyavraj2204@gmail.com>
2025-09-16 13:32:58 +0530

    superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,23 +19,6 @@ echo "Migrations applied!"
 python manage.py collectstatic --noinput
 echo "Static files collected!"
 
-# Create superuser if it doesn't exist
-python manage.py shell <<EOF
-import os
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
-email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpass")
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superuser created!")
-else:
-    print("Superuser already exists.")
-EOF
-
 
 # Start Gunicorn server
 exec gunicorn visual_generator_tool.wsgi:application --bind 0.0.0.0:8002 --workers 3
