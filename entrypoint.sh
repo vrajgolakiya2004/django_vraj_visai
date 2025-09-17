84f2b88098c237c44c784141e864a87e78ec0abe
MarketingBot <golakiyavraj2204@gmail.com>
2025-09-17 15:57:38 +0530

    DB_HOST

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e  # exit if any command fails
 
-echo "Waiting for PostgreSQL at 69.62.78.167:5432..."
+echo "Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
 
-until pg_isready -h "69.62.78.167" -p "5432" -U "postgres"; do
+until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" do
   sleep 2
 done
 
